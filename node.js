949e54cba4a109c08b19b64ee9eae2a305db67b7
gajdascz <gajdascz@gmail.com>
2023-12-19 21:28:49 -0500

    Add linked list node factory logic
    - Adds `name`, `data`, and `next` parameters
      - `name` for reference, `data` for storage, `next` for linking nodes
    - Implements methods:
      - `get next()`, `set next(nextNode)`, `get allData()`, getName(),
      `setName(newName)`, `addData(key)`, `removeData(key)`, `getData(key)`,

---
+++ node.js
@@ -0,0 +1,57 @@
+function node(name = null, data = {}, next = null) {
+  let _data = data;
+  let _next = next;
+  let _name = name;
+
+  const hasDataKey = (key) => Object.keys(_data).includes(key);
+
+  const addData = (key, value) => {
+    try {
+      if (hasDataKey(key)) throw new Error(`${key} already exists.`);
+      else _data[key] = value;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const removeData = (key) => {
+    try {
+      if (!hasDataKey(key)) throw new Error(`${key} does not exist.`);
+      else delete _data[key];
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getData = (key) => {
+    try {
+      if (!hasDataKey(key)) throw new Error(`${key} does not exist.`);
+      else return _data[key];
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return {
+    get next() {
+      return _next;
+    },
+    get allData() {
+      return _data;
+    },
+    set next(nextNode) {
+      _next = nextNode;
+    },
+    get name() {
+      return _name;
+    },
+    set name(newName) {
+      _name = newName;
+    },
+    addData,
+    removeData,
+    getData,
+  };
+}
+
+export { node };
